de65c351a20d76d1008ba1f53a3760588ede8185
Weiming Chen <mingstar215@gmail.com>
2016-07-25 11:36:38 +1000

    Remember tensorflow session in the current thread along with the graph

--- sample_server.py
+++ sample_server.py
@@ -13,12 +13,14 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=int, default='8080',
                        help='port the server runs on')
-    parser.add_argument('--environment', type=str, default='development',
-                       help="environment the server runs in, e.g. 'development', 'production'")
-    parser.add_argument('--model_dir', type=int, default='save',
+    parser.add_argument('--production', action="store_true",
+                        help="specify whether the server runs in production environment or not")
+    parser.add_argument('--model_dir', type=str, default='save',
                        help='directory to restore checkpointed models')
     args = parser.parse_args()
-    server_config = {'server.socket_port': args.port, 'environment': args.environement}
+    server_config = {'server.socket_port': args.port}
+    if args.production:
+        server_config['environment'] = 'production'
     cherrypy.config.update(server_config)
     cherrypy.quickstart(SampleServer(args.model_dir))
 
@@ -30,35 +32,37 @@ class SampleServer(object):
         self.threaded_models = {}
         self._load()
 
-    def load(self):
+    def _load(self):
         with open(os.path.join(self.model_dir, 'chars_vocab.pkl'), 'rb') as f:
             self.chars, self.vocab = cPickle.load(f)
         with open(os.path.join(self.model_dir, 'config.pkl'), 'rb') as f:
             self.saved_args = cPickle.load(f)
 
-    def _get_thread_model(self):
+    def _get_model_for_current_thread(self):
         tid = threading.get_ident()
         if tid in self.threaded_models:
             return self.threaded_models[tid]
         # else
         self.lock.acquire()
         if not tid in self.threaded_models:
-            self.threaded_models[tid] = Model(self.saved_args, infer=True)
+            model = Model(self.saved_args, infer=True)
+            session = tf.Session()
+            with session.as_default():
+                tf.initialize_all_variables().run()
+                saver = tf.train.Saver(tf.all_variables())
+                ckpt = tf.train.get_checkpoint_state(self.model_dir)
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(session, ckpt.model_checkpoint_path)
+                    print("Loaded {}".format(ckpt.model_checkpoint_path))
+            self.threaded_models[tid] = (model, session)
         self.lock.release()
         return self.threaded_models[tid]
 
     @cherrypy.expose
     def index(self, prime='The ', n=200, sample_mode=2):
-        model = self._get_thread_model()
-        with tf.Session() as sess:
-            tf.initialize_all_variables().run()
-            saver = tf.train.Saver(tf.all_variables())
-            ckpt = tf.train.get_checkpoint_state(self.model_dir)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
-                print("Loaded {}".format(ckpt.model_checkpoint_path))
-                result = model.sample(sess, self.chars, self.vocab, n, prime, sample_mode)
-                return result
+        model, session = self._get_model_for_current_thread()
+        with session.as_default():
+            return model.sample(session, self.chars, self.vocab, n, prime, sample_mode)
 
 
 if __name__ == '__main__':
